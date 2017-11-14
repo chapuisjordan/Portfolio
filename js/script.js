7d19791c72d3cf19022823e3deeb6d3c1823e9f1
Jordan <jordanchapuis@MacBook-Air-de-SEGOUN.local>
2017-11-14 19:07:31 +0100

    Slide responsive, scrollTop valeur en pourcentage

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 $(document).ready(function(){
   $(window).on('scroll', function(){
     var heightScroll = Math.round($(window).scrollTop());
-    var perc = $(document).height();
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var scrollPercent =(docHeight - winHeight);
 
     //Partie à propos de moi.
-    if (heightScroll > perc *0.16){
+    if (heightScroll > scrollPercent * 0.2){
       $('.arrow-hide-moi').css('transform', 'translateX(-100%)');
       $('.arrow-hide-moi').css('transition', '1.5s');
     }
-    if (heightScroll > perc *0.40 || heightScroll < perc *0.15){
+    if (heightScroll > scrollPercent * 0.5|| heightScroll < scrollPercent * 0.15){
       $('.arrow-hide-moi').css('transform', 'translateX(0%)');
     }
 
     //Partie mes projets.
-    if (heightScroll > perc *0.40){
+    if (heightScroll > scrollPercent * 0.5){
       $('.arrow-hide-projets').css('transform', 'translateX(-100%)');
       $('.arrow-hide-projets').css('transition', '1.5s');
     }
-    if (heightScroll > perc *0.57 || heightScroll < perc *0.35){
+    if (heightScroll > scrollPercent * 0.82 || heightScroll < scrollPercent * 0.45){
       $('.arrow-hide-projets').css('transform', 'translateX(0%)');
     }
 
     //Partie mes compétences.
-    if (heightScroll > perc *0.57){
+    if (heightScroll > scrollPercent * 0.80){
       $('.arrow-hide-competences').css('transform','translateX(-100%)');
       $('.arrow-hide-competences').css('transition','1.5s');
     }
-    if (heightScroll < perc *0.55 || heightScroll > perc *0.99){
+    if (heightScroll < scrollPercent * 0.75 || heightScroll > scrollPercent * 0.95){
       $('.arrow-hide-competences').css('transform','translateX(0%)');
     }
 
